249af86027dca1fffd2659952ba700b1cc6e4c0f
judkins8 <joshua.judkins@gmail.com>
2023-02-13 19:54:18 -0700

    First Commit

---
+++ Mission_6/Models/ErrorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mission_6.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
+
---
+++ Mission_6/Models/MovieDatabaseContext.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mission_6.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Threading.Tasks;
+
+namespace Mission_6.Models
+{
+    public class MovieDatabaseContext : DbContext
+    {
+        //Constructor
+        public MovieDatabaseContext(DbContextOptions<MovieDatabaseContext> options) : base(options)
+        {
+            //Leave blank for now
+        }
+
+        public DbSet<AppRes> Responses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<AppRes>().HasData(
+                new AppRes
+                {
+                    ApplicationID = 1,
+                    Category = "Action/Adventure",
+                    Title = "Thor Ragnarok",
+                    Year = 2017,
+                    Director = "Taika Waititi",
+                    Rating = "PG-13"
+                },
+                new AppRes
+                {
+                    ApplicationID = 2,
+                    Category = "Sci-Fi",
+                    Title = "Tenet",
+                    Year = 2020,
+                    Director = "Christopher Nolan",
+                    Rating = "PG-13"
+                },
+                new AppRes
+                {
+                    ApplicationID = 3,
+                    Category = "Comedy",
+                    Title = "The Lego Batman Movie",
+                    Year = 2017,
+                    Director = "Chris McKay",
+                    Rating = "PG"
+                }
+            );
+        }
+    }
+}
